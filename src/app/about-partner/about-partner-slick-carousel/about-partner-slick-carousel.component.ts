dc5c5fc819301c3f746cdc837075898f176ea447
Tiago.Antonio <tiago.antonio@mv.com.br>
2023-12-12 07:57:15 -0300

    [MASTER-17] Cotinuação do projeto one page adicionando o swiper e slick e configurando.

--- src/app/about-partner/about-partner-slick-carousel/about-partner-slick-carousel.component.ts
+++ src/app/about-partner/about-partner-slick-carousel/about-partner-slick-carousel.component.ts
@@ -9,14 +9,22 @@ import Swiper from 'swiper';
 export class AboutPartnerSlickCarouselComponent implements OnInit{
 
   slickConfig = {
-    slidesToShow: 3, // Número de itens visíveis ao mesmo tempo
-    slidesToScroll: 1, // Número de itens a serem movidos por vez
-    autoplay: true, // Ativa a reprodução automática
-    autoplaySpeed: 2000, // Velocidade da reprodução automática em milissegundos
-    infinite: true, // Habilita o movimento contínuo do carrossel
+    // slidesToShow: 3, // Número de itens visíveis ao mesmo tempo
+    // slidesToScroll: 1, // Número de itens a serem movidos por vez
+    // autoplay: true, // Ativa a reprodução automática
+    // autoplaySpeed: 2000, // Velocidade da reprodução automática em milissegundos
+    // infinite: true, // Habilita o movimento contínuo do carrossel
+
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 200,
+    Infinity: true
   };
 
-  itens = [
+  slides = [
     { id: 1, imagem: 'assets/genesis-foto/veicular.jpeg' },
     { id: 2, imagem: 'assets/genesis-foto/veicular.jpeg' },
     { id: 3, imagem: 'assets/genesis-foto/veicular.jpeg' },
@@ -26,26 +34,59 @@ export class AboutPartnerSlickCarouselComponent implements OnInit{
     { id: 8, imagem: 'assets/genesis-foto/bradesco-saude.png' },
     { id: 9, imagem: 'assets/genesis-foto/veicular.jpeg' },
 
+
     // Adicione mais itens conforme necessário
   ];
 
+  slideConfig = {
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000};
+
+  addSlide() {
+    this.slides.push({id: 10, imagem: "assets/genesis-foto/veicular.jpeg"})
+  }
+
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+
+  slickInit(event: any) {
+    console.log('slick initialized');
+  }
+
+  breakpoint(event: any) {
+    console.log('breakpoint');
+  }
+
+  afterChange(event: any) {
+    console.log('afterChange');
+  }
+
+  beforeChange(event: any) {
+    console.log('beforeChange');
+  }
+
   ngOnInit(): void {
 
-    document.addEventListener("DOMContentLoaded", function () {
-      const swiper = new Swiper('.swiper-container', {
-        // Configurações opcionais
-        slidesPerView: 5,
-        spaceBetween: 5,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-        },
-      });
-    });
+    // document.addEventListener("DOMContentLoaded", function () {
+    //   const swiper = new Swiper('.swiper-container', {
+    //     // Configurações opcionais
+    //     slidesPerView: 5,
+    //     spaceBetween: 5,
+    //     navigation: {
+    //       nextEl: '.swiper-button-next',
+    //       prevEl: '.swiper-button-prev',
+    //     },
+    //     pagination: {
+    //       el: '.swiper-pagination',
+    //       clickable: true,
+    //     },
+    //   });
+    // });
 
   }
 
