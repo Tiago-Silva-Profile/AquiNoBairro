3581224341b39fdcf6a1f646668ca961adb79b64
Tiago.Antonio <tiago.antonio@mv.com.br>
2023-12-22 13:43:37 -0300

    [MASTER-26] Ajuste nos filtros do menu da pagina dos parceiros.

--- src/app/about-partner/about-partner-menu/about-partner-menu.component.ts
+++ src/app/about-partner/about-partner-menu/about-partner-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-partner-menu',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-partner-menu.component.css']
 })
 export class AboutPartnerMenuComponent {
+
+  productService:any[] = [
+    {service:'Planos de Saúde', caminho:'/partner/seguradoras'},
+    {service:'Planos Odontologicos', caminho:'/partner/seguradoras'},
+    {service:'Planos de Saúde Pet', caminho:'/partner/seguradoras'},
+    {service:'Seguro Veícular', caminho:'/partner/seguradoras'},
+    {service:'Seguro de Vida', caminho:'/partner/seguradoras'},
+  ]
+
+  //Sempre que a navegação for concluída, a página será rolada para o topo.
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { AboutPartnerComponent } from './about-partner/about-partner.component';
 import { AboutPartnerWeComponent } from './about-partner/about-partner-we/about-partner-we.component';
 import { AboutPartnerStartComponent } from './about-partner/about-partner-start/about-partner-start.component';
 import { AboutPartnerProductsPortfolioComponent } from './about-partner/about-partner-products-portfolio/about-partner-products-portfolio.component';
+import { AboutPartnerContactComponent } from './about-partner/about-partner-contact/about-partner-contact.component';
 
 
 const routes: Routes = [
   { path:'', component: AboutPartnerStartComponent },
   { path:'partner/about', component: AboutPartnerWeComponent },
   { path:'partner/seguradoras', component: AboutPartnerProductsPortfolioComponent },
+  { path:'partner/contatos', component: AboutPartnerContactComponent },
   // ... outras rotas, se houver
 ];
 
