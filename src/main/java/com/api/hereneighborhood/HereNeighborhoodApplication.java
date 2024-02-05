e79ca71468f9e923e1450b23700bc3fc6030d6b5
Tiago.Antonio <tiago.antonio@mv.com.br>
2024-02-05 13:06:50 -0300

    "[HERE-01] Startando o projeto aqui no bairro no backend"

---
+++ src/main/java/com/api/hereneighborhood/HereNeighborhoodApplication.java
@@ -0,0 +1,13 @@
+package com.api.hereneighborhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereNeighborhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereNeighborhoodApplication.class, args);
+	}
+
+}
